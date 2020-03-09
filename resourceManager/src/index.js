7033c770deca5d77af48935b37c63dc98e84bf17
YouyouyouScu <252392434@qq.com>
2020-03-09 23:04:34 +0800

    resourceManager

--- resourceManager/src/index.js
+++ resourceManager/src/index.js
@@ -1,6 +1,7 @@
 import _ from 'lodash';
 import './style.css';
-import Icon from './icon.jpg';
+//import Icon from './icon.jpg';
+import Data from './data.xml';
 
 function component() {
     var element = document.createElement('div');
@@ -9,11 +10,11 @@ function component() {
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('hello');
     // 将图像添加到我们现有的 div。
-    var myIcon = new Image();
-    myIcon.src = Icon;
+    // var myIcon = new Image();
+    // myIcon.src = Icon;
 
-    element.appendChild(myIcon);
-  
+    // element.appendChild(myIcon);
+    console.log(Data);
     return element;
   }
   
